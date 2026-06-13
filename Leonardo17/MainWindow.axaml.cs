97a2784916fa34465dbb69ac7a120d216c3e73be
Illuminedsauna@gmail.com <Mattia26111985>
2026-06-13 11:56:56 +0200

    Aggiungere i file di progetto.

---
+++ Leonardo17/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Leonardo17
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
